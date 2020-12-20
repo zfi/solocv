61bc0a8c0c94ce2752fb430135127c2d5b682b88
Jim Ewald <jim.ewald@gmail.com>
2020-12-20 11:13:15 -0800

    Install tailwindcss support. Update base view to validate tailwindcss is working.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div align="center">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
 <script>
