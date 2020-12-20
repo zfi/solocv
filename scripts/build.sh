b60c04be67384689720eb141cc7fb73f68ace9e4
Jim Ewald <jim.ewald@gmail.com>
2020-12-20 08:55:27 -0800

    Update scripts to use latest image tag.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,8 @@
 #
 # Build solo-cv image.
 # -----------------------------------------------
+tag_version=5
 
-docker build --platform arm64 -t solocv-dev:4 .
+docker build --platform arm64 -t solocv-dev:$tag_version .
+docker tag solocv-dev:$tag_version solocv-dev:latest
+
